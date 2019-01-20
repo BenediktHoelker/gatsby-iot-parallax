92068c0e4ab9edd62d527572b66f00effcc002c6
Benedikt Hölker <benedikt.hoelker@gmail.com>
2019-01-20 17:02:28 +0100

    Styles for the body

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,32 @@
-import {Link} from 'gatsby'
+import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
-import styles from "./header.module.css"
+import styles from './header.module.css'
 
-const Header = ({siteTitle, menuLinks}) => (
+const Header = ({ siteTitle, menuLinks }) => (
   <div className={styles.header}>
     <nav className={styles.navigation}>
       {menuLinks.map(link => {
-        return <li key={link.name} className={styles.listItem}>
-          <h1>
-            <Link className={styles.link}
-              to={link.link}>{link.name}</Link>
-          </h1>
-        </li>;
-      })
-}
+        return (
+          <li key={link.name} className={styles.listItem}>
+            <h1>
+              <Link className={styles.link} to={link.link}>
+                {link.name}
+              </Link>
+            </h1>
+          </li>
+        )
+      })}
     </nav>
   </div>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string
+  siteTitle: PropTypes.string,
 }
 
 Header.defaultProps = {
-  siteTitle: ``
+  siteTitle: ``,
 }
 
 export default Header
