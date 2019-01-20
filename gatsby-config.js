aa7e961996f222066c6f66824fa0acf26bc9c172
Benedikt Hölker <benedikt.hoelker@gmail.com>
2019-01-20 14:23:49 +0100

    Show a customized header with dynamic links

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,13 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
+    menuLinks: [{
+      name: `Home`,
+      link: `/`
+    }, {
+      name: `Page2`,
+      link: `/page-2`
+    }]
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,49 @@
-import { Link } from 'gatsby'
+import {Link} from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
+const Header = ({siteTitle, menuLinks}) => (
+  <div style={{
+    background: 'DodgerBlue',
+    marginBottom: `1.45rem`
+  }}>
     <div
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+      margin: `0 auto`,
+      maxWidth: 960
+    }}>
+      <nav style={{
+        display: 'flex',
+        flex: 1
+      }}>
+        {menuLinks.map(link => <li
+          key={link.name}
           style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+          listStyleType: 'none',
+          textAlign: 'center',
+          padding: '14px 16px'
+        }}>
+          <h1>
+            <Link
+              style={{
+              color: `white`,
+              textDecoration: `none`
+            }}
+              to={link.link}>{link.name}</Link>
+          </h1>
+        </li>)
+}
+      </nav>
     </div>
   </div>
 )
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
+  siteTitle: PropTypes.string
 }
 
 Header.defaultProps = {
-  siteTitle: ``,
+  siteTitle: ``
 }
 
 export default Header
