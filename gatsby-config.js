f516cdaf13e4101eba831cfcc51ddc5ad1618349
Benedikt Hölker <benedikt.hoelker@gmail.com>
2019-01-20 16:07:28 +0100

    Add Typography

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,26 +3,26 @@ module.exports = {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
-    menuLinks: [{
-      name: `Home`,
-      link: `/`
-    }, {
-      name: `Page2`,
-      link: `/page-2`
-    }]
+    menuLinks: [
+      {
+        name: `Home`,
+        link: `/`
+      }, {
+        name: `Page2`,
+        link: `/page-2`
+      }
+    ]
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
+    `gatsby-plugin-react-helmet`, {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
+    `gatsby-plugin-sharp`, {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
@@ -32,10 +32,14 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
-  ],
+      }
+    }, {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`
+      }
+    }
+    // this (optional) plugin enables Progressive Web App + Offline functionality To
+    // learn more, visit: https://gatsby.app/offline 'gatsby-plugin-offline',
+  ]
 }
