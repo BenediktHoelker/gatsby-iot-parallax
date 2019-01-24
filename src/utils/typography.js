c65fea0c1479027b42133d5949e3d2c1b1159d47
Benedikt Hölker <benedikt.hoelker@gmail.com>
2019-01-24 19:07:51 +0100

    Update typography.js

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,5 @@
 import Typography from "typography"
 import grandViewTheme from "typography-theme-grand-view"
-import wordpress2016Theme from "typography-theme-wordPress-2016"
 
 const typography = new Typography(grandViewTheme)
 
